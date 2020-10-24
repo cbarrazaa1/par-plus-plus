b4bb9e2cb0b07fd991d007b933d940e29b335361
Cesar Barraza <cbarrazaa1@gmail.com>
2020-10-23 19:42:05 -0500

    added comments and some code cleanup

--- src/ParseTreeListener.ts
+++ src/ParseTreeListener.ts
@@ -1,4 +1,4 @@
-import { Stack } from 'typescript-collections';
+import {Stack} from 'typescript-collections';
 import {ParPlusPlusListener} from './antlr/ParPlusPlusListener';
 import {
   Addsub_opContext,
@@ -303,145 +303,193 @@ export default class Listener implements ParPlusPlusListener {
   }
 
   exitIf_expr(): void {
+    // make sure it's int for bool check
     if (getTypeForAddress(this.quads.operands.peek()) != ValueType.INT) {
       throw new Error('Type mismatch in if-statement');
     }
 
+    // create incomplete gotof
     this.quads.create(
       QuadrupleAction.GOTOF,
       this.quads.operands.pop(),
       null,
       null,
     );
+
+    // store migaja de pan
     this.quads.jumps.push(this.quads.size() - 1);
   }
 
   exitIf_stmt(): void {
     const end = this.quads.jumps.pop();
     const size = this.quads.size();
+
+    // fill else goto
     this.quads.fill(end, size);
   }
 
   enterElse_stmt(ctx: Else_stmtContext): void {
+    // if there's no else, do nothing
     if (ctx.block() == null) {
       return;
     }
-    
+
+    // create incomplete goto to end
     this.quads.create(QuadrupleAction.GOTO, null, null, null);
 
     const end = this.quads.jumps.pop();
     const size = this.quads.size();
+
+    // store current quad to fill later
     this.quads.jumps.push(size - 1);
+
+    // fill gotof jump
     this.quads.fill(end, size);
   }
 
   exitOutput_arg(ctx: Output_argContext): void {
-    const string = ctx.STR_VAL();
+    const res =
+      ctx.STR_VAL() != null
+        ? this.memory.newString()
+        : this.quads.operands.pop();
 
-    if (string != null) {
-      this.quads.create(
-        QuadrupleAction.WRITE, 
-        null, 
-        null, 
-        this.memory.newString(),
-      );
-    } else {
-      this.quads.create(
-        QuadrupleAction.WRITE, 
-        null, 
-        null, 
-        this.quads.operands.pop());
-    }
+    this.quads.create(QuadrupleAction.WRITE, null, null, res);
   }
 
   exitInput_args(ctx: Input_argsContext): void {
     const ids = ctx.var_id();
 
+    // create read quad for every argument
     for (let i = 0; i < ids.length; i++) {
       const variable = this.getVariable(ids[i].text);
-      this.quads.create(
-        QuadrupleAction.READ, 
-        null, 
-        null, 
-        variable.addr);
+      this.quads.create(QuadrupleAction.READ, null, null, variable.addr);
     }
   }
 
-  enterWhile_stmt(ctx: While_stmtContext): void {
+  enterWhile_stmt(): void {
+    // store migaja de pan
     this.quads.jumps.push(this.quads.size());
   }
-  
-  exitWhile_stmt(ctx: While_stmtContext): void {
+
+  exitWhile_stmt(): void {
     const end = this.quads.jumps.pop();
     const ret = this.quads.jumps.pop();
+
+    // goto while start
     this.quads.create(QuadrupleAction.GOTO, null, null, ret);
+
+    // fill initial gotof
     this.quads.fill(end, this.quads.size());
   }
-  
-  exitWhile_expr(ctx: While_exprContext): void {
+
+  exitWhile_expr(): void {
     const result = this.quads.operands.pop();
     const type = getTypeForAddress(result);
 
     // check if types match
     if (type != ValueType.INT) {
       throw new Error('Type mismatch');
-    } else {
-      this.quads.create(QuadrupleAction.GOTOF, result, null, null);
-      this.quads.jumps.push(this.quads.size() - 1);
     }
+
+    // create incomplete quad for exiting while
+    this.quads.create(QuadrupleAction.GOTOF, result, null, null);
+    this.quads.jumps.push(this.quads.size() - 1);
   }
 
   exitFor_id(ctx: For_idContext): void {
     const variable = this.getVariable(ctx.var_id().text);
     const type = getTypeForAddress(variable.addr);
 
+    // make sure control variable is a number
     if (type != ValueType.INT) {
       throw new Error('Type mismatch');
     }
 
+    // store control variable
     this.quads.operands.push(variable.addr);
   }
 
-  exitFor_expr1(ctx: For_expr1Context): void {
+  exitFor_expr1(): void {
     const exp = this.quads.operands.pop();
     const type = getTypeForAddress(exp);
-    if (type != ValueType.INT && type != ValueType.FLOAT){
+
+    // check if correct type
+    if (type != ValueType.INT) {
       throw new Error('Type mismatch');
-    } else {
-      const controlVar = this.quads.operands.peek();
-      const controlType = getTypeForAddress(this.quads.operands.peek());
-      const resultType = SemanticCube[controlType][type][Op.ASSIGN];
-      if (resultType == null){
-        throw new Error('Type mismatch');
-      } else {
-        this.quads.create(QuadrupleAction.ASSIGN, exp, null, this.quads.operands.peek());
-      }
     }
+
+    // make sure control variable is compatible with first expression
+    const controlVar = this.quads.operands.peek();
+    const controlType = getTypeForAddress(controlVar);
+    const resultType = SemanticCube[controlType][type][Op.ASSIGN];
     
+    // check if assign is possible
+    if (resultType == null) {
+      throw new Error('Type mismatch');
+    }
+
+    // assign initial exp to control var
+    this.quads.create(
+      QuadrupleAction.ASSIGN,
+      exp,
+      null,
+      controlVar,
+    );
   }
 
-  exitFor_expr2(ctx: For_expr2Context): void {
+  exitFor_expr2(): void {
     const exp = this.quads.operands.pop();
+
+    // make sure expression is compatible type
     if (getTypeForAddress(exp) != ValueType.INT) {
       throw new Error('Type mismatch.');
     }
 
+    // create temporal so we can store comparison result
     const temp = this.memory.newInt(MemoryType.Temp);
-    this.quads.create(QuadrupleAction.LT, this.quads.operands.peek(), exp, temp);
+
+    // create comparison quad
+    this.quads.create(
+      QuadrupleAction.LT,
+      this.quads.operands.peek(),
+      exp,
+      temp,
+    );
+
+    // create migaja de pan for comparison at end of for
     this.quads.jumps.push(this.quads.size() - 1);
+
+    // create incomplete gotof to end of for
     this.quads.create(QuadrupleAction.GOTOF, temp, null, null);
+
+    // create miagaja de pan to fill gotof later
     this.quads.jumps.push(this.quads.size() - 1);
   }
 
-  exitFor_stmt(ctx: For_stmtContext): void {
+  exitFor_stmt(): void {
     const controlVar = this.quads.operands.pop();
+
+    // create temp variable to store addition result
     const temp = this.memory.newInt(MemoryType.Temp);
-    this.quads.create(QuadrupleAction.ADD, controlVar, this.memory.newInt(MemoryType.Constant), temp);
+
+    // add one to control var
+    this.quads.create(
+      QuadrupleAction.ADD,
+      controlVar,
+      this.memory.newInt(MemoryType.Constant), // 1
+      temp,
+    );
+
+    // update control var
     this.quads.create(QuadrupleAction.ASSIGN, temp, null, controlVar);
 
     const end = this.quads.jumps.pop();
     const ret = this.quads.jumps.pop();
+
+    // create goto to return to comparison
     this.quads.create(QuadrupleAction.GOTO, null, null, ret);
+
+    // fill initial gotof of for
     this.quads.fill(end, this.quads.size());
   }
 }
